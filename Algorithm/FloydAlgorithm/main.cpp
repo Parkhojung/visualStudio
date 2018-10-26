39a5a867513c103cacb9d28b3dc228d21edd3d9b
phj <parkhj062@gmail.com>
2018-10-26 22:51:23 +0900

    경유하는 경로를 알려주는 path() 추가

--- Algorithm/FloydAlgorithm/main.cpp
+++ Algorithm/FloydAlgorithm/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void matPrn(vector<vector<int>> A, char* matName);
 int minimum(int a, int b);
+void path(vector<vector<int>> P, int q, int r);
 int main() {
 
 	fstream fin("input.txt");
@@ -38,7 +39,7 @@ int main() {
 		for (i = 0; i < N; i++) {
 			for (j = 0; j < N; j++) {
 				if (D[i][k] + D[k][j] < D[i][j]) {
-					P[i][j] = k+1;
+					P[i][j] = k+1;				// P 행렬에는 1부터 N으로 저장하여 보기 좋도록 함.
 					D[i][j] = D[i][k] + D[k][j];
 				}
 			}
@@ -47,6 +48,10 @@ int main() {
 
 	matPrn(D,"D");
 	matPrn(P,"P");
+	
+	cout << "path :";
+	path(P, 5-1, 3-1); // 실제 행렬 인덱스는 0부터 시작하므로.
+	cout << endl;
 	return 0;
 }
 
@@ -70,7 +75,7 @@ void matPrn(vector<vector<int>> A, char* matName) {
 	cout << endl;
 
 	for (int i = 0; i < A.size(); i++) {
-		cout << "l"<<setw(blockWidth) << i + 1 << setw(blockWidth)<<"l";
+		cout << "l"<<setw(blockWidth) << i << setw(blockWidth)<<"l";
 
 		for (int j = 0; j < A.size(); j++) {
 			cout << setw(fontWidth) << A[i][j] ;
@@ -78,6 +83,8 @@ void matPrn(vector<vector<int>> A, char* matName) {
 
 		cout << setw(blockWidth)<< right<<"l"<< endl;
 	}
+
+
 	for (int i = 0; i <= A.size()*fontWidth + blockWidth * 3; i++) {
 		cout << "-";
 	}
@@ -90,5 +97,15 @@ int minimum(int a, int b) {
 	}
 	else {
 		return a;
+	}
+}
+
+void path(vector<vector<int>> P, int q, int r) {
+	q = q ;
+	r = r ;
+	if (P[q][r] != 0) {
+		path(P,q, P[q][r]-1); // 인덱스 참조시 -1
+		cout << "v" << (P[q][r]) << " "; 
+		path(P, P[q][r]-1, r);
 	}
 }
